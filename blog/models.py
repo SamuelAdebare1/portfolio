25a9cc9a6cd5e95d9d8dc91a290e63f2c6b1d747
joser-world <samueladebare1010.gmail.com>
2019-04-02 01:27:51 +0300

    All blogs

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Blog(models.Model):
 	title=models.CharField(max_length=50)
-	pub_date = models.DateTimeField(auto_now=True)
+	pub_date = models.DateTimeField()
 	body=models.TextField(max_length=200)
 	image=models.ImageField(upload_to="images/")
