22a8ec883283dd7e1b021d8c6ea198498d83bf07
joser-world <samueladebare1010.gmail.com>
2019-04-01 19:01:13 +0300

    Ours

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,11 +77,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "portfoliodb",
-		"USER": "postgres",
-		"PASSWORD": "git531457",
-		"HOST": "localhost",
-		"PORT": "5432",
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
